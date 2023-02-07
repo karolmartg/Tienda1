80aad55385089bd6e92217152ef33f51c495e940
karolmartg <karolmartgamb@hotmail.com>
2023-02-06 18:41:55 -0600

    Tienda 1.

---
+++ src/test/java/com/Tienda1/demo/Tienda1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tienda1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
